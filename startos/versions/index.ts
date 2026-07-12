d85b66395bedebb2fa56dc848e14da40cda82234
William K⚡Santiago🔑☢️ <william@privkey.io>
2026-07-11 20:49:49 -0400

    Bump readstr to 985ab25 and release 0.1.0:3 (#6)

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -2,8 +2,9 @@ import { VersionGraph } from '@start9labs/start-sdk'
 import { v0_1_0_0 } from './v0.1.0_0'
 import { v0_1_0_1 } from './v0.1.0_1'
 import { v0_1_0_2 } from './v0.1.0_2'
+import { v0_1_0_3 } from './v0.1.0_3'
 
 export const versionGraph = VersionGraph.of({
-  current: v0_1_0_2,
-  other: [v0_1_0_0, v0_1_0_1],
+  current: v0_1_0_3,
+  other: [v0_1_0_0, v0_1_0_1, v0_1_0_2],
 })
